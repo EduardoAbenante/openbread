c39fa1019eef60362b8409b5877e51ee816c3ed1
EduardoAbenante <ejabenante@alumnat.copernic.cat>
2026-06-01 22:54:18 +0200

    react frontend

---
+++ openbread-backend/openbread/src/test/kotlin/com/eab/openbread/OpenbreadApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.eab.openbread
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OpenbreadApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
