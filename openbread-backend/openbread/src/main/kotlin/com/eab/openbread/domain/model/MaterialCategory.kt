51ead7dc912740ab77b6480647598c0b5cbda31a
EduardoAbenante <ejabenante@alumnat.copernic.cat>
2026-06-08 19:01:23 +0200

    creacion entidades materiaspriemras y categoriaMateriaprimera

---
+++ openbread-backend/openbread/src/main/kotlin/com/eab/openbread/domain/model/MaterialCategory.kt
@@ -0,0 +1,26 @@
+package com.eab.openbread.domain.model
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "MATERIAL_CATEGORIES")
+class MaterialCategory(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    @Column(unique = true, length = 50)
+    val name: String,
+
+    @Column(length = 200)
+    val description: String? = null,
+
+    val active: Boolean = true
+
+)
---
+++ openbread-backend/openbread/src/main/kotlin/com/eab/openbread/domain/model/RawMaterial.kt
@@ -0,0 +1,32 @@
+package com.eab.openbread.domain.model
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "RAW_MATERIALS")
+class RawMaterial(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    @Column(unique = true, length = 50)
+    val name: String,
+
+    @Column(length = 200)
+    val description: String? = null,
+
+    val active: Boolean = true,
+
+    val photoUrl: String? = null,
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    var category: MaterialCategory? = null
+)
