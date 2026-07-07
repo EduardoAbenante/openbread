4e84fd31f7ac0a40aeaff423b329ab87cca7d597
Eduardo Abenante <eduabenantec@hotmail.com>
2026-07-07 16:23:44 +0200

    añadir configuracion para tests

--- openbread-backend/openbread/build.gradle.kts
+++ openbread-backend/openbread/build.gradle.kts
@@ -40,7 +40,8 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    runtimeOnly("com.h2database:h2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
