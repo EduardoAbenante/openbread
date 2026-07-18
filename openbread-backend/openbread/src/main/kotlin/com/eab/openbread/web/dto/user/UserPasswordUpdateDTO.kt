12208c874ec98de4b130dd802403cb51dad8eed0
EduardoAbenante <ejabenante@alumnat.copernic.cat>
2026-07-18 16:48:17 +0200

    corrección regex vontraseña DTO, actuwalizacion gitignore

--- openbread-backend/openbread/src/main/kotlin/com/eab/openbread/web/dto/user/UserPasswordUpdateDTO.kt
+++ openbread-backend/openbread/src/main/kotlin/com/eab/openbread/web/dto/user/UserPasswordUpdateDTO.kt
@@ -16,7 +16,7 @@ data class UserPasswordUpdateDTO(
     @field:NotBlank(message = "{val.user.password.empty}")
     @field:Size(min = 6, max = 100, message = "{val.user.password.size}")
     @field:Pattern(
-        regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@\\$!%*#?&])[A-Za-z\\d@\\$!%*#?&]{6,}\\$",
+        regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@\\$!%*#?&])[A-Za-z\\d@\\$!%*#?&]{6,}$",
         message = "{val.user.password.strength}"
     )
     val password: String
